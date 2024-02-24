fc104440fef7b6b124b1c3882cd1fd1792228b44
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-02-24 16:03:31 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n1 = int(input('Insira um número inteiro:'))
+dobro = n1 * 2
+triplo = n1 * 3
+raiz_quadrada = n1 ** (1/2)
+print('Você inseriu o número {}.\nO dobro de {} é {}.\nO triplo de {} é {}.\nA raiz quadrada de {} é {}'.format(n1, n1, dobro, n1, triplo, n1, raiz_quadrada))
